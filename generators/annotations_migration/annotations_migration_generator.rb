1e87633891096fada8da25d812bab0be2fb69373
Jits <web@jits.co.uk>
2009-07-23 18:27:08 +0100

    Updated the db migrations generator to allow for versioned db migrate scripts (on order to make it easy to upgrade the plugin in the future).

--- generators/annotations_migration/annotations_migration_generator.rb
+++ generators/annotations_migration/annotations_migration_generator.rb
@@ -1,11 +1,26 @@
-class AnnotationsMigrationGenerator < Rails::Generator::Base 
-  def manifest 
-    record do |m| 
-      m.migration_template 'migration.rb', 'db/migrate' 
-    end 
+class AnnotationsMigrationGenerator < Rails::Generator::Base
+
+  attr_accessor :version
+
+  def initialize(*runtime_args)
+    super(*runtime_args)
+    if @args[0].nil?
+      @version = "v1"
+    else
+      @version = @args[0].downcase
+    end
   end
-  
+
+  def manifest
+    record do |m|
+      if @version
+        m.migration_template "migration_#{@version}.rb", 'db/migrate'
+      end
+    end
+  end
+
   def file_name
-    "annotations_migration"
+    "annotations_migration_#{@version}"
   end
+
 end
