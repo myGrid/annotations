39f1face5d4db0e0f8062b4514f206f5f558da76
quyen <nttqa22001@yahoo.com>
2013-04-26 14:21:12 +0200

    replace some deprecated methods

--- rails/init.rb
+++ rails/init.rb
@@ -2,7 +2,7 @@ require 'annotations.rb'
 
 # FIX for engines model reloading issue in development mode
 if ENV['RAILS_ENV'] != 'production'
-	load_paths.each do |path|
-		ActiveSupport::Dependencies.load_once_paths.delete(path)
+	autoload_paths.each do |path|
+		ActiveSupport::Dependencies.autoload_once_paths.delete(path)
 	end
 end
