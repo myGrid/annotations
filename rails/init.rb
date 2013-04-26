e6b2595eb5449260c150e8202d94fb47cbe9a58b
quyen <nttqa22001@yahoo.com>
2013-04-26 14:45:20 +0200

    fix load_paths error

--- rails/init.rb
+++ rails/init.rb
@@ -2,7 +2,7 @@ require 'annotations.rb'
 
 # FIX for engines model reloading issue in development mode
 if ENV['RAILS_ENV'] != 'production'
-	autoload_paths.each do |path|
+	load_paths.each do |path|
 		ActiveSupport::Dependencies.autoload_once_paths.delete(path)
 	end
 end
